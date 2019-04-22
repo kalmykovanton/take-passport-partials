c5069c35fb4aec67a6cfb058007916cd5f4d7ead
Anton Kalmykov <a.kalmykov@mobidev.biz>
2019-04-22 16:38:18 +0300

    Updates

---
+++ precache-manifest.79fed208f41d8a3dd0e3ace9ee2c759d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c89424e4ed367e9af6d",
+    "url": "/take-passport-partials/static/js/runtime~main.d860c401.js"
+  },
+  {
+    "revision": "2353d8d910f4114a1d7e",
+    "url": "/take-passport-partials/static/js/main.d9a965d1.chunk.js"
+  },
+  {
+    "revision": "000591050a381b2f2e61",
+    "url": "/take-passport-partials/static/js/2.2f27c26c.chunk.js"
+  },
+  {
+    "revision": "2353d8d910f4114a1d7e",
+    "url": "/take-passport-partials/static/css/main.77e5d4d2.chunk.css"
+  },
+  {
+    "revision": "000591050a381b2f2e61",
+    "url": "/take-passport-partials/static/css/2.3768bfb3.chunk.css"
+  },
+  {
+    "revision": "72641b5cfc49d200f5d362700a07d296",
+    "url": "/take-passport-partials/index.html"
+  }
+];
